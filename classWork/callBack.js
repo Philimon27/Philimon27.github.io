b67d1363c8afda523a1a3abe09a1e32c027fecfb
Filimon Bihonegn <100728745+Philimon27@users.noreply.github.com>
2022-03-09 16:31:39 -0600

    d33 call context

--- classWork/callBack.js
+++ classWork/callBack.js
@@ -1,30 +1,51 @@
-       "use strict"
-// • write a function, multiplyEvens, that can be called with any number of arguments and returns the product of the even arguments
-// • do first using the arguments object • then using ...rest parameter
-     // using argument objects
-  function multiplyEvens(){
-      let result=1;
-         for(const element of arguments){
-             if(element%2==0){
-                 result*=element;
-             }
-         }
-       return result;
-  }
-  multiplyEvens(3,6,12)
-  console.log(multiplyEvens(3,6,12))// output is 72
+//        "use strict"
+// // • write a function, multiplyEvens, that can be called with any number of arguments and returns the product of the even arguments
+// // • do first using the arguments object • then using ...rest parameter
+//      // using argument objects
+//   function multiplyEvens(){
+//       let result=1;
+//          for(const element of arguments){
+//              if(element%2==0){
+//                  result*=element;
+//              }
+//          }
+//        return result;
+//   }
+//   multiplyEvens(3,6,12)
+//   console.log(multiplyEvens(3,6,12))// output is 72
 
-  //using rest parameter
+//   //using rest parameter
 
-  function multiplyyEvens(...rest){
-    let result=1;
-       for(let i=0;i<rest.length;i++){
-           if(rest[i]%2==0){
-               result*=rest[i];
-           }
-       }
-     return result;
+//   function multiplyyEvens(...rest){
+//     let result=1;
+//        for(let i=0;i<rest.length;i++){
+//            if(rest[i]%2==0){
+//                result*=rest[i];
+//            }
+//        }
+//      return result;
+// }
+
+// multiplyyEvens(3,6,12)
+// console.log(multiplyEvens(3,6,12))//output is 72
+
+
+
+//"use strict"; 
+
+// // function a() {
+// // console.log(this);   
+// // }
+const b = {
+dog: 'fido',
+log: function() {
+    
+console.log(this); }
 }
 
-multiplyyEvens(3,6,12)
-console.log(multiplyEvens(3,6,12))//output is 72
+// console.log(this); 
+// a();
+// b.log();
+
+let mylog = b.log; 
+mylog();
