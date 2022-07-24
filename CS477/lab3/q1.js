982cf62939e624d7276eeb93d6535344021316cb
Filimon Bihonegn <100728745+Philimon27@users.noreply.github.com>
2022-07-24 00:30:20 -0500

    lab3

--- CS477/lab3/q1.js
+++ CS477/lab3/q1.js
@@ -1,12 +1,38 @@
-
-const file=require()
+const fs = require('fs');
+const { dirname } = require('path');
+const path=require('path')
 const http= require('http').createServer((req,res)=>{
     
    
     if(req.url==='/'&& req.method==='GET'){
-  
+       fs.createReadStream(path.join(__dirname, 'submitForm.html')).pipe(res)
     }
     else if(req.url==='/submit' && req.method=='POST'){
+        const body = [];
 
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const str = Buffer.concat(body).toString();
+            console.log(str);
+            fs.writeFile('users.txt', str, (err) => {
+                if (!err) {
+                  
+                    res.end(`<body>
+                              Save succesfully
+                             <a href ='/'> homepage</a>
+                             </body>`);
+
+                } else {
+                    res.end('Try again ');
+                }
+            })
+        });
+    } else {
+        res.end('others....');
     }
+
+    
 }).listen(3000);
